a4fb2f68c4f3d10d438c1956bd8da5e4a87708b8
SamZhou <sam@mixmedia.com>
2019-04-15 11:46:20 +0800

    resize page 2 A4 size

--- render/pdf.js
+++ render/pdf.js
@@ -11,25 +11,19 @@ if (system.args.length > 2) {
 }
 
 page.paperSize = {
-    width: '1240px', 
-    height: '1754px', 
-    margin: '0px'
+    margin: '0px',
+    format: "A4"
 }
-/* page.clipRect = { 
-    top: 0, 
-    left: 0, 
-    width: 1240, 
-    height: 1754 
-};
- */
 page.zoomFactor = 1;
-
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
         phantom.exit(1);
     } else {
         window.setTimeout(function () {
+            page.evaluate(function(){
+                document.body.style.zoom = 0.48;
+            });
             page.render(output, {format: 'pdf', quality: '10'});
             phantom.exit();
         }, 200);
